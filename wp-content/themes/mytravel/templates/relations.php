2f42ea8d472ee047cd1e6472f1c2bd2fa48ee212
Tatsiana-prog <126953436+Tatsiana-prog@users.noreply.github.com>
2023-11-18 11:30:57 +0300

    Update relations.php

--- wp-content/themes/mytravel/templates/relations.php
+++ wp-content/themes/mytravel/templates/relations.php
@@ -10,7 +10,7 @@ Template Name: relations
 			<h1 class="heading" data-aos="zoom-out">контакты</h1>
 			 <div class="row">
 				<div class="map" data-aos="flip-left">
-                        		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2243.5510964671284!2d49.12341731295015!3d55.783667972983466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x415ead0f7f18fd83%3A0x806ad71c92b7ab9!2z0YPQuy4g0J7RgdGC0YDQvtCy0YHQutC-0LPQviwgNjcsINCa0LDQt9Cw0L3RjCwg0KDQtdGB0L8uINCi0LDRgtCw0YDRgdGC0LDQvSwg0KDQvtGB0YHQuNGPLCA0MjAxMDc!5e0!3m2!1sru!2sby!4v1700216087355!5m2!1sru!2sby" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2243.5510964671284!2d49.12341731295015!3d55.783667972983466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x415ead0f7f18fd83%3A0x806ad71c92b7ab9!2z0YPQuy4g0J7RgdGC0YDQvtCy0YHQutC-0LPQviwgNjcsINCa0LDQt9Cw0L3RjCwg0KDQtdGB0L8uINCi0LDRgtCw0YDRgdGC0LDQvSwg0KDQvtGB0YHQuNGPLCA0MjAxMDc!5e0!3m2!1sru!2sby!4v1700216087355!5m2!1sru!2sby" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         	</div>
 				<div class="form" data-aos="flip-right">
     					<form action="mailto:programvtv07@gmail.com" method="post">
